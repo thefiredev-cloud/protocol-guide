77a2dc580429a8849663a29da8982b41af77d4b8
Tanner Osterkamp <tanner-osterkamp@Tanners-MacBook-Pro.local>
2026-01-08 18:37:46 -0800

    chore: Update index.ts
    Auto-committed by Claude Code after file modification

---
+++ data/library/curriculum/index.ts
@@ -0,0 +1,11 @@
+import { complicatedDelivery } from './curriculum-complicated-delivery';
+import { humeralIO } from './curriculum-humeral-io';
+import { needleT } from './curriculum-needle-t';
+import { quickTransfuser } from './curriculum-quick-transfuser';
+
+export const curriculumProtocolsList = [
+  complicatedDelivery,
+  humeralIO,
+  needleT,
+  quickTransfuser
+];
