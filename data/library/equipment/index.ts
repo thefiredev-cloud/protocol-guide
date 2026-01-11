921b45b8a131e45f17228c0e8b2b85d7491e2692
Tanner Osterkamp <tanner-osterkamp@Tanners-MacBook-Pro.local>
2026-01-10 16:08:27 -0800

    chore: Update index.ts
    Auto-committed by Claude Code after file modification

---
+++ data/library/equipment/index.ts
@@ -0,0 +1,24 @@
+import { sofTourniquet } from './sof-tourniquet';
+import { russellChestSeal } from './russell-chest-seal';
+import { celoxGauze } from './celox-gauze';
+import { slishmanSplint } from './slishman-splint';
+import { igelAirway } from './igel-airway';
+import { cpapEtco2 } from './cpap-etco2';
+
+export const equipmentProtocols = [
+  sofTourniquet,
+  russellChestSeal,
+  celoxGauze,
+  slishmanSplint,
+  igelAirway,
+  cpapEtco2,
+];
+
+export {
+  sofTourniquet,
+  russellChestSeal,
+  celoxGauze,
+  slishmanSplint,
+  igelAirway,
+  cpapEtco2,
+};
